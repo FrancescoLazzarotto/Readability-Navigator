4e0c462ea5cd224789f1c665df57e003ea39278c
Francesco Lazzarotto <82331651+FrancescoLazzarotto@users.noreply.github.com>
2025-11-14 11:15:28 +0100

    Add files via upload

---
+++ src/features/embeddings.py
@@ -0,0 +1,61 @@
+import pandas as pd 
+import numpy as np
+from sentence_transformers import SentenceTransformer
+from sklearn.cluster import KMeans
+from collections import Counter
+from utils.io_utils import load_data, save_pickle, load_pickle
+
+
+path = r"C:\Users\checc\OneDrive\Desktop\readability-navigator\data\processed\onestop_nltk_features.csv"
+try: 
+    dataframe = load_data(path)
+except FileNotFoundError:
+    raise FileNotFoundError(f"File non trovato nel path {path}")
+
+sentences = dataframe['testo']
+
+
+def model_embedding():
+    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    return model 
+
+model = model_embedding()
+
+def sentences_embedding(sentences, model):
+    
+    embedding = model.encode(
+        sentences,
+        truncate_dim = 512
+                            )
+    return embedding
+
+def similarity_embedding(embedding, model):     
+    similarity = model.similarity(embedding, embedding)
+    return similarity
+
+def clustering(df, embedding, k = 10, random_state = 42):
+    kmeans = KMeans(n_clusters=k, random_state = random_state)
+    labels = kmeans.fit_predict(embedding)
+    df["topic_cluster"] = labels
+    return df, kmeans
+
+def cluster_label(df, k):
+    description = {}
+    
+    for clusters_id in range(k):
+        text = df[df["topic_cluster"]] == clusters_id["testo"]
+        words = " ".join(text).lower().split()
+        common = []
+        for index, word in Counter(words).most_common(10):
+            description[clusters_id] = common 
+    
+    return description 
+        
+
+embedding = sentences_embedding(sentences, model)
+similarity = similarity_embedding(embedding, model)
+save_pickle('doc_embedding.pickle', embedding)
+save_pickle('similarity_embedding.pickle', similarity)
+
+
+
---
+++ src/user/model_user.py
@@ -0,0 +1,24 @@
+import numpy as np 
+
+def user_model(user_id, chosen_cluster, df, embedding, default_readability = 60):
+    
+    embedding  = []
+    
+    for cluster in chosen_cluster:
+        id = df[df["topic_cluster"] == cluster].index
+        emb = np.mean(embedding[id], axis= 0)
+        embedding.append(emb)
+    
+    topic_vector = np.mean(embedding, axis=0)
+     
+    profile = {
+        "user_id": user_id,
+        "topic_vector": topic_vector.tolist(),
+        "target_readability": default_readability,
+        "chosen_topics": chosen_cluster,
+        "history": {}
+    }
+    
+    return profile
+
+
