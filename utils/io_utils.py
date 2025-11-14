ba69b6b2648fe3ce3c7e7d741547d4bba00cc842
Francesco Lazzarotto <82331651+FrancescoLazzarotto@users.noreply.github.com>
2025-11-14 11:17:03 +0100

    Update io_utils.py

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -1,7 +1,7 @@
 import json
-import yaml
 import pandas as pd
 import os
+import pickle
 
 def load_json(path):
     with open(path, "r", encoding="utf-8") as f:
@@ -12,13 +12,19 @@ def save_json(data, path):
     with open(path, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
-def load_yaml(path):
-    with open(path, "r", encoding="utf-8") as f:
-        return yaml.safe_load(f)
-
 def load_csv(path):
     return pd.read_csv(path, encoding="utf-8")
 
 def save_csv(df, path):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     df.to_csv(path, index=False, encoding="utf-8")
+
+def save_pickle(name_file, data):
+    with open(name_file, "wb") as pkl:
+        pickle.dump(data, pkl)
+
+def load_pickle(name_file, data):
+    with open(name_file, 'rb') as pkl:
+        data = pickle.load(pkl)
+    return data
+        
