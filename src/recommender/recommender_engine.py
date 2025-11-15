df1dc637a49ef4d949ed769e6f3c8f435fe0f56b
Francesco Lazzarotto <checcolazzarotto@gmail.com>
2025-11-15 15:56:58 +0100

    class rec engine, methods implementation, not all implemented

--- src/recommender/recommender_engine.py
+++ src/recommender/recommender_engine.py
@@ -4,15 +4,22 @@ import numpy as np
 from user.model_user import user_model
 from utils.io_utils import load_yamal, load_json, save_json, load_pickle
 from sklearn.metrics.pairwise import cosine_similarity
+from utils.config_loader import load_config 
+
 
 
 class RecommenderEngine():
-    def __init__(self, df, embedding, config, user_id, profile_path):
+    def __init__(self, df, embedding, config, user_id, profile_path, flusch_ease):
         self.df = df
         self.embedding = embedding
-        self.config = config
+        #self.config = config
         self.user_id = user_id
         self.profile_path = profile_path
+        self.flusch = flusch_ease
+    
+    def yamal(self):
+        config = load_config()
+        return config 
 
     def profile(self):
         if os.path.exists(self.profile_path):
@@ -36,10 +43,24 @@ class RecommenderEngine():
         emb = self.embedding[idx]
         return testo, emb
 
-    def get_fusch(self):
-        return NotImplementedError
+    def get_flesch(self, doc_id):
+         idx = self.df.index[self.df["id"] == doc_id[0]]
+         flesch = self.df.loc[idx, "flesch_score"]
+         return flesch
+         
+    def gap_readability(self, user, flesch):
+        target_readability = user['target_readability']
+        readability = flesch 
+        gap = abs(target_readability - readability) 
+        return gap, target_readability, readability 
     
-    
+    def penality(self):
+        if self.target_readability < self.readability:
+            score_penality = True
+        elif self.target_readability > self.readability:
+            score_penality = False 
+        return score_penality
+        
     def theme_similarity(self, user, doc_id):
         topic_vector = np.array(user['topic_vector']).reshape(1, -1)
         _, emb = self.get_document(doc_id)
@@ -47,3 +68,21 @@ class RecommenderEngine():
         sim_score = cosine_similarity(topic_vector, emb)[0][0]
         return sim_score
         
+    def recommender(self, user, doc_id, flesch):
+        config = self.yamal()
+        sim = self.theme_similarity(user, doc_id)
+        gap = self.gap_readability5(user, flesch)
+        k = config['k']
+        eta = config['eta']
+        zeta = config['zeta']
+        score = eta * sim - zeta  * gap
+        return score         
+
+
+        
+
+    
+
+    
+    
+    
